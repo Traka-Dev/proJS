c260b9be45a7d9c2792cace8203409ee1704e431
Traka Dev <trakadev@gmail.com>
2020-12-29 02:07:29 -0600

    js cache handler

--- assets/index.js
+++ assets/index.js
@@ -16,3 +16,9 @@ button.onclick = () => player.togglePlay();
 
 const button_mute = document.getElementById("mute_btn");
 button_mute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
