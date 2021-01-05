32ac12e46bdcb4c642158bdf8ebb1a2545baa9a1
Traka Dev <trakadev@gmail.com>
2021-01-04 22:25:21 -0600

    Se actualizo a typescript el proyecto

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlayer.js";
-import AutoPause from "./plugins/AutoPause.js"
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./plugins/AutoPlay";
+import AutoPause from "./plugins/AutoPause";
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
@@ -11,10 +11,10 @@ const player = new MediaPlayer({
     ]
 });
 
-const button = document.querySelector("button");
+const button: HTMLElement = document.querySelector("button");
 button.onclick = () => player.togglePlay();
 
-const button_mute = document.getElementById("mute_btn");
+const button_mute: HTMLElement = document.getElementById("mute_btn");
 button_mute.onclick = () => player.toggleMute();
 
 if ('serviceWorker' in navigator) {
